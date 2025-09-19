0607f4e79cfe601a688032b78d1d76bd349c1844
shamsur-rahman-rifat <shamsur30rahman@gmail.com>
2025-09-19 12:24:37 +0600

    Updated front end api

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5090/api',
+  baseURL: 'https://blog-management-khan-it.onrender.com/api',
   timeout: 10000,
 });
 
